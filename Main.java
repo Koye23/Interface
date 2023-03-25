9cc88b714a134e9ef634d83bdd8f0d172ea32ef0
koyecoding2 <128574239+Koye23@users.noreply.github.com>
2023-03-25 01:13:57 +0000

    This is from Replit

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("this is typing from codespace.");
-    
+    System.out.println("this is from replit.");
   }
 }
